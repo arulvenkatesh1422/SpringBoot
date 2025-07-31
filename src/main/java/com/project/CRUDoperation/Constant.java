fe4749596e67cb1b70623b4d8bcf3de63780dd33
arulvenkatesh1422 <aarulvenkatesh@gmail.com>
2025-07-31 21:30:09 +0530

    changes

--- src/main/java/com/project/CRUDoperation/Constant.java
+++ src/main/java/com/project/CRUDoperation/Constant.java
@@ -2,7 +2,7 @@ package com.project.CRUDoperation;
 
 public class Constant {
 	
-	//validation implemented
+	//validation implemented here
 	public static final String Resource_Not_f="student id is already exists...!!!"; 
 	public static final String Resource_Not_present="student Id not found...!!!";
 	public static final String Id_DID_Not_Macth="Id Did Not Match For The Record";
